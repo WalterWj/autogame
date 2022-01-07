8cc12304d1ce17f18646e2cd07758feca617fb53
walterwj <277372664@qq.com>
2022-01-07 20:50:32 +0800

    main

--- commend/check.py
+++ commend/check.py
@@ -7,7 +7,19 @@ from default import color_check, write_log
 
 
 # 房间检查
-def home_close(x, y):
+# con_x,con_y,con_c: 继续位置 x,y 坐标和颜色，如果多次 41，说明已经刷完
+# bell_nomal: 没有铃铛的颜色，默认 222
+# bell_un_0:铃铛异常状态 --> 有阴影，默认为：239
+# bell_un_1:铃铛异常状态 --> 有阴影，默认为：231
+# bell_x, bell_y:铃铛位置
+def home_close(con_x,
+               con_y,
+               con_c,
+               bell_x,
+               bell_y,
+               bell_nomal=222,
+               bell_un_0=239,
+               bell_un_1=231):
     # 游戏时间
     start_time = time.time()
     # 等待房间刷完
@@ -19,11 +31,11 @@ def home_close(x, y):
         # 3s 检查一次
         time.sleep(3)
         # 控制时间，如果超过一定时间也会 break
-        c_d = color_check(x, y)
+        c_d = color_check(con_x, con_y)
         # print("捕获继续位置颜色：{},{},{}".format(x, y, c_d))
-        write_log(content="捕获继续位置颜色：{},{},{}".format(x, y, c_d))
+        write_log(content="捕获继续位置颜色：{},{},{}".format(con_x, con_y, c_d))
         # 控制 41 像素连续
-        if c_d == 41:
+        if c_d == con_c:
             nc += 1
         else:
             nc = 0
@@ -38,24 +50,24 @@ def home_close(x, y):
         wc += 1
         # 过了 8min，还没有退出
         if wc >= 160:
-            # print("Game abnormal")
-            write_log(content="Game abnormal")
+            write_log(content="Game abnormal~")
             break
         else:
             pass
-        # 检查如果铃铛正常，也退出
+        # 60s 后检查如果铃铛正常，也退出
         # 铃铛颜色
         if wc >= 20:
             time.sleep(1)
-            a_cd = color_check(x=1362, y=57)
+            a_cd = color_check(x=bell_x, y=bell_y)
             # print("铃铛颜色:{}".format(a_cd))
             write_log(content="铃铛颜色:{}".format(a_cd))
             # 如果 2min 后，发现铃铛灰色，说明异常，退出房间。
-            if a_cd == 222 or a_cd == 239:
+            if a_cd == bell_nomal:
                 break
             # 如果房间解散，直接点击 g 返回界面
-            if a_cd == 231:
+            if a_cd == bell_un_0 or a_cd == bell_un_1:
                 pyautogui.press('g')
+
         numb = "已经游戏 {} s".format(3 * wc)
         print(numb, end='')
         print('\b' * len(numb) * 2, end='', flush=True)
@@ -65,3 +77,4 @@ def home_close(x, y):
     print("--- cost {} seconds ---".format((time.time() - start_time)))
     write_log(content="--- cost {} seconds ---".format((time.time() -
                                                         start_time)))
+
--- commend/default.py
+++ commend/default.py
@@ -23,3 +23,21 @@ def write_log(file="main.log", content="", mod='a+'):
     # 添加日志，做分析使用
     with open(file, mod, encoding='utf-8') as f:
         f.write(content)
+
+    content = "Write file {}, Contennt: {}".format(file, content)
+
+    return content
+
+
+def ConOrQuit(click_x, click_y, while_num=8):
+    numc = 0
+    while numc <= while_num:
+        numc += 1
+        time.sleep(1.5)
+        pyautogui.click(x=click_x, y=click_y)
+
+    content = "点击 x:{},y:{} {} 次, 耗时 {}s".format(click_x, click_y, while_num,
+                                                 while_num * 1.5)
+
+    return content
+
